4cea6cee238a7f0f688c47298b97278b234e870f
chris off <christian.hg.hegi@gmail.com>
2023-06-21 11:35:17 +0900

    with request in actionCreator and better css

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -2,35 +2,42 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: { items: [], totalQuantity: 0, totalAmount: 0 },
+  initialState: { items: [], totalQuantity: 0, totalAmount: 0, changed: false },
   reducers: {
+    replaceCart(state, action) {
+      state.items = action.payload.items;
+      state.totalQuantity = action.payload.totalQuantity;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload;
       const exisitngItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!exisitngItem) {
         state.items.push({
           id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title
+          name: newItem.title,
         });
-      }else{
-        exisitngItem.quantity ++;
+      } else {
+        exisitngItem.quantity++;
         exisitngItem.totalPrice = exisitngItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
-        const id = action.payload;
-        const exisitingItem = state.items.find(item => item.id === id);
-        state.totalQuantity--;
-        if(exisitingItem.quantity === 1){
-            state.items = state.items.filter(item => item.id !== id);
-        } else{
-            exisitingItem.quantity--;
-            exisitingItem.totalPrice = exisitingItem.totalPrice - exisitingItem.price;
-        }
+      const id = action.payload;
+      const exisitingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      state.changed = true;
+      if (exisitingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        exisitingItem.quantity--;
+        exisitingItem.totalPrice = exisitingItem.totalPrice - exisitingItem.price;
+      }
     },
   },
 });
